c8ee01491707be5a5c89667cb3e6558feb7edc92
dilanithathsarani <dilanithathsarani2003@gmail.com>
2026-06-08 21:10:51 +0530

    project initialized and folder structure created

---
+++ src/routes/appRoutes.jsx
@@ -0,0 +1,19 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+function AppRoutes() {
+    return (
+        <BrowserRouter>
+            <Routes>
+
+                <Route path="/" element={<h1>Home</h1>} />
+
+                <Route path="/login" element={<h1>Login</h1>} />
+
+                <Route path="/register" element={<h1>Register</h1>} />
+
+            </Routes>
+        </BrowserRouter>
+    );
+}
+
+export default AppRoutes;
