76554aa16919b3e2911627172082aaa0e6f2bab8
dilanithathsarani <dilanithathsarani2003@gmail.com>
2026-06-09 13:32:13 +0530

    footer file created

---
+++ src/components/footer.jsx
@@ -0,0 +1,25 @@
+function Footer() {
+
+    return (
+
+        <footer className="bg-gray-900 text-white py-6 mt-10">
+
+            <div className="max-w-7xl mx-auto text-center">
+
+                <h2 className="font-bold">
+                    AI Job Portal
+                </h2>
+
+                <p>
+                    © 2026 All Rights Reserved
+                </p>
+
+            </div>
+
+        </footer>
+
+    );
+
+}
+
+export default Footer;
